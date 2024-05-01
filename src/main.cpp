ed4d7762254c979b6df74e58d8db248490c01b92
Renaud Laviolette-Harvengt <renaudlavioletteharvengt@gmail.com>
2024-04-30 21:57:11 -0400

    added basic joystick control

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,14 @@ DHT_Unified dht(sensorsPins.dht, dhtValues.dhtType);
 hd44780_I2Cexp lcd(lcdValues.address);
 Ultrasonic ultrasonic(sensorsPins.ultrasonicTrig, sensorsPins.ultrasonicEcho);
 
+// other variables
+int currentCursorY = 0;
+bool joystickMoved = false;
+
 // function declarations
 void setupLCD();
 void setupJoystick();
+void handleJoystickControl();
 
 void setup() {
     Serial.begin(115200);  
@@ -46,20 +51,48 @@ void setup() {
 }
 
 void loop() {    
+    handleJoystickControl();    
 }
 
 // function definitions
-void setupLCD()
-{
+void setupLCD() {
     Wire.begin(lcdPins.sda, lcdPins.scl);
     lcd.begin(lcdValues.cols, lcdValues.rows);
     lcd.backlight();
     lcd.blink();    
 }
 
-void setupJoystick()
-{
-    pinMode(joystickPins.SW, INPUT);
+void setupJoystick() {
+    pinMode(joystickPins.SW, INPUT_PULLUP);
     pinMode(joystickPins.VRx, INPUT);
     pinMode(joystickPins.VRy, INPUT);
+}
+
+void handleJoystickControl() {
+    // margin for center return and center
+    int joystickCenter = 2449;
+    int joystickCenterMargin = 1000;    
+
+    // read the current joystick position, baseline is approx. 2449
+    int joystickY = analogRead(joystickPins.VRy);
+    
+    // reset joystick moving if is near the center
+    if (abs(joystickY - joystickCenter) <= joystickCenterMargin) {
+        joystickMoved = false;
+    }
+
+    // move cursor up if joystick moved up
+    if (joystickY < joystickCenter - joystickCenterMargin && currentCursorY > 0 && !joystickMoved) {
+        currentCursorY--;
+        joystickMoved = true;
+    }
+
+    // move cursor down if joystick moved down
+    if (joystickY > joystickCenter + joystickCenterMargin && currentCursorY < 3 && !joystickMoved) {
+        currentCursorY++;
+        joystickMoved = true;
+    }
+
+    // move lcd cursor
+    lcd.setCursor(0, currentCursorY);
 }
