497c5fed4e87d103af5d7bdf92def1ca19320ce7
Renaud Laviolette-Harvengt <renaudlavioletteharvengt@gmail.com>
2025-03-14 14:21:55 -0400

    display class

---
+++ include/Display.h
@@ -0,0 +1,36 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <Wire.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+#include "Logger.h"
+
+// Struct to hold LCD configuration values
+struct LCDValues {
+    uint8_t address;
+    uint8_t cols;  
+    uint8_t rows;    
+};
+
+// Struct to hold LCD pin configuration
+struct LCDPins {
+    uint8_t sda;     
+    uint8_t scl;
+};
+
+class Display {
+private:
+    LCDValues _lcdValues;
+    LCDPins _lcdPins;
+    hd44780_I2Cexp _lcd;        
+
+public:
+    Display();
+
+    bool setup();
+    void clearRow(int row);
+    void clearScreen();
+};
+
+#endif
---
+++ src/display.cpp
@@ -0,0 +1,47 @@
+#include "Display.h"
+
+Display::Display() :
+    _lcdValues{0x27, 20, 4},
+    _lcdPins{10, 11},
+    _lcd(_lcdValues.address)
+{}
+
+bool Display::setup() {
+    Wire.begin(_lcdPins.sda, _lcdPins.scl);
+
+    int lcdInitStatus = _lcd.begin(_lcdValues.cols, _lcdValues.rows);
+
+    if (lcdInitStatus != 0) {
+        Logger::getLogger()->log(INFO, "Setting up display");
+
+        bool wireInitStatus = Wire.begin(_lcdPins.sda, _lcdPins.scl);        
+        if (!wireInitStatus) {
+            Logger::getLogger()->log(ERROR, "LCD: Wire did not initialize correctly");
+            return false;
+        }
+
+        int lcdInitStatus = _lcd.begin(_lcdValues.cols, _lcdValues.rows);
+        if (lcdInitStatus != 0) {
+            Logger::getLogger()->log(ERROR, "LCD did not initialize correctly.");
+            return false;
+        }
+        _lcd.backlight();
+        _lcd.blink();        
+        return true;
+    }
+}
+
+void Display::clearRow(int row) {
+    for (size_t i = 0; i < _lcdValues.cols; i++)
+    {
+        _lcd.setCursor(i, row);
+        _lcd.print(" ");
+    }    
+}
+
+void Display::clearScreen() {
+    for (size_t i = 0; i < _lcdValues.rows; i++)
+    {
+        clearRow(i);
+    }    
+}
